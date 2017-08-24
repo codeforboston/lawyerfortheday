bd11549dc8a89c2af2055c7631851a0ca0d8c03f
Dominic Surrao <dsurrao@Dominics-MacBook-Pro.local>
2017-08-24 14:02:15 -0400

    clear repo

--- data_entry/static/js/backbone/collections/organizations.js
+++
@@ -1,5 +0,0 @@
-var Organizations = Backbone.Collection.extend({
-  model: Organization,
-  url: 'http://api.open.referral.adopta.agency/organizations/',
-  //url: 'http://api.open.referral.adopta.agency/organizations/?page=1&per_page=4',
-});
